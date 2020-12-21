a707c5a9e8497964dc3ae2d21461261af69ce549
Artur Babiński <artur.babinski.g@gmail.com>
2020-12-21 16:53:59 +0100

    review and beauty rock, paper, scissors game

--- kurs_zrozumieć_programowanie/rock, paper, scissors.py
+++ kurs_zrozumieć_programowanie/rock, paper, scissors.py
@@ -1,10 +1,23 @@
 import random
+
+# licznik punktów
 player_points = 0
 computer_points = 0
-list = ["rock", "paper", "scissors"]
+
+list_of_posible_computer_choice = ["rock", "paper", "scissors"]
+
+print("Witam Cię w grze rock/paper/scissors! Zasady są następujące")
+print("Musisz wpisać jedną z trzech opcji: rock/paper/scissors")
+print("Po twoim wyborze zostanie rozegrana runda.")
+print("Na koniec każdej rundy zostanie podanu wynik obu graczy (twój i komputera")
+print("Gramy do trzech wygranych rund. Jeśli któryś z graczy osiągnie ten wynik, gra się zakończy i zostanie podany zwycięzca")
+print("Dobrej zabawy :)\n")
 while player_points < 3 and computer_points < 3:
     player = input("rock/paper/scissors?: ")
-    computer = random.choice(list)
+    if player != "rock" and player != "paper" and player != "scissors":
+        print("oj. Musiałeś coś źle wpisać, Spróbuj raz jeszcze wpisać jedno z następujących:\n rock, paper lub scissors")
+        continue
+    computer = random.choice(list_of_posible_computer_choice)
     print("players:", player, "vs", "computers:", computer)
     if player == "rock":
         if computer == "paper":
@@ -21,8 +34,13 @@ while player_points < 3 and computer_points < 3:
             computer_points += 1
         if computer == "paper":
             player_points += 1
-
     print("player:", player_points, ", computer:", computer_points)
-print("Dzięki za grę!")
 
+
+
+if player_points > computer_points:
+    print("\nGratuluje, wygrałeś!")
+else:
+    print("\nNiestety ale tym razem wygrał komputer...")
+print("Dzięki za grę!")
 
