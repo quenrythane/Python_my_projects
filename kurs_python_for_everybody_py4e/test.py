25bdd1e05f4522e1fc2738bc029d56140c27caee
Artur Babiński <artur.babinski.g@gmail.com>
2020-12-24 15:49:56 +0100

    new commit on windows cmd

--- kurs_python_for_everybody_py4e/test.py
+++ kurs_python_for_everybody_py4e/test.py
@@ -2,3 +2,7 @@
 # 2nd attempt
 # 3th attempt
 # 4th attempts
+# 5th attempt. Now ssh working. I had to use redirection from https to ssh.
+# git remote set-url origin git@github.com:quenrythane/Python_my_projects.git
+
+# tak tak
