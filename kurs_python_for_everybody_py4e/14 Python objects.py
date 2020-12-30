089c7dd571e0a70801ccefa86a19e59bc16f13f7
Artur Babiński <artur.babinski.g@gmail.com>
2020-12-30 03:31:40 +0100

    Chapter 15 databases

--- kurs_python_for_everybody_py4e/14 Python objects.py
+++ kurs_python_for_everybody_py4e/14 Python objects.py
@@ -1,18 +1,28 @@
 class PartyAnimal:
     x = 0
     name = ''
+
     def __init__(self, nam):
         self.name = nam
-        print(self.name,'constructed')
+        print(self.name, 'constructed')
 
-    def party(self) :
+    def party(self):
         self.x = self.x + 1
-        print(self.name,'party count',self.x)
+        print(self.name, 'party count', self.x)
 
 
-s = PartyAnimal('Sally')
-j = PartyAnimal('Jim')
+class CricketFan(PartyAnimal):
+    points = 0
 
+    def six(self):
+        self.points = self.points + 7
+        self.party()
+        print(self.name, "points", self.points)
+
+
+s = PartyAnimal("Sally")
 s.party()
+
+j = CricketFan("Jim")
 j.party()
-s.party()
+j.six()
---
+++ kurs_python_for_everybody_py4e/15 databases 1.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+# this gonna create a file with name in ""
+conn = sqlite3.connect("emaildb.sqlite")
+cur = conn.cursor()
+
+# it just delete table if exist (but should not exist yet)
+cur.execute("DROP TABLE IF EXISTS Counts")
+
+cur.execute("CREATE TABLE Counts (email TEXT, count INTEGER)")
+
+fname = input("Enter file name: ")
+if len(fname) < 1: fname = 'mbox-short.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith("From: "):
+        continue
+    pieces = line.split()   
+    email = pieces[1]
+    # this ask mark (?) is only placeholder
+    # protect from SQL injection attacks
+    cur.execute("SELECT count FROM Counts WHERE email = ? ", (email, ))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute("INSERT INTO Counts (email, count) VALUES (?, 1)", (email, ))
+    else:
+        cur.execute("INSERT INTO Counts SET count = count + 1 WHERE email = ?", (email, ))
+    conn.commit()
+
+# https://sqlite.org/lang/select.html
+sqlstr = "SELECT email, count FROM Count ORDER BY count DESC LIMIT 10"
+
+for row in cur.execute(sqlstr):
+    print()
